798a82e22c655e600763ea548fe6de2f3a239e87
Caleb Kissinger <kaylubkiss@gmail.com>
2024-07-26 00:17:45 -0500

    some icons now in video game tab

--- Javascript/navigation.js
+++ Javascript/navigation.js
@@ -26,7 +26,7 @@ class SecondaryPageNavHeader extends HTMLElement
         <div class='header_grid'>\
             <div class='grid_attrib' onclick=\"document.location='calebswebpage_home.html'\">Home</div>\
             <div class='grid_attrib' onclick=\"document.location='calebswebpage_secondpage.html'\">About</div>\
-            <div class='grid_attrib'>Video Games</div>\
+            <div class='grid_attrib'onclick=\"document.location='calebswebpage_videogames_homepage.html'\">Video Games</div>\
             <div class='grid_attrib'>Animation</div>\
             <div class='grid_attrib'>Video Production</div>\
         </div>\
