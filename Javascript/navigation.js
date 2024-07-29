7b2616649ebfb5f3dc83aa04d09a50cacae75fc3
Kaylubkiss <Kaylubkiss@users.noreply.github.com>
2024-07-29 13:01:38 -0600

    crude write-ups of video games section of my website. needs to be styled

--- Javascript/navigation.js
+++ Javascript/navigation.js
@@ -6,8 +6,8 @@ class HomeNavigationHeader extends HTMLElement
         <link rel='stylesheet' href='stylesheets/headerframe.css'>\
         \
         <div class='header_grid'>\
-        <div class='grid_attrib' onclick=\"document.location='calebswebpage_secondpage.html'\">About</div>\
-            <div class='grid_attrib' onclick=\"document.location='calebswebpage_videogames_homepage.html'\">Video Games</div>\
+        <div class='grid_attrib'>About</div>\
+            <div class='grid_attrib' onclick=\"document.location='calebswebpage_videogames_homepage.html'\">Programming</div>\
             <div class='grid_attrib'>Animation</div>\
             <div class='grid_attrib'>Video Production</div>\
         </div>"
@@ -25,8 +25,8 @@ class SecondaryPageNavHeader extends HTMLElement
         <link rel='stylesheet' href='stylesheets/headerframe.css'>\
         <div class='header_grid'>\
             <div class='grid_attrib' onclick=\"document.location='calebswebpage_home.html'\">Home</div>\
-            <div class='grid_attrib' onclick=\"document.location='calebswebpage_secondpage.html'\">About</div>\
-            <div class='grid_attrib'onclick=\"document.location='calebswebpage_videogames_homepage.html'\">Video Games</div>\
+            <div class='grid_attrib'>About</div>\
+            <div class='grid_attrib'onclick=\"document.location='calebswebpage_videogames_homepage.html'\">Programming</div>\
             <div class='grid_attrib'>Animation</div>\
             <div class='grid_attrib'>Video Production</div>\
         </div>\
