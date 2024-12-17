a9a925115750edafec61d3e02bcc2f307c2958af
Caleb Kissinger <kaylubkiss@gmail.com>
2024-12-17 02:55:59 -0600

    fixed navigation button sending to null html reference

--- Javascript/navigation.js
+++ Javascript/navigation.js
@@ -24,7 +24,7 @@ class SecondaryPageNavHeader extends HTMLElement
         this.innerHTML = "\
         <link rel='stylesheet' href='stylesheets/headerframe.css'>\
         <div class='header_grid'>\
-            <div class='grid_attrib' onclick=\"document.location='home.html'\">Home</div>\
+            <div class='grid_attrib' onclick=\"document.location='index.html'\">Home</div>\
             <div class='grid_attrib'>About</div>\
             <div class='grid_attrib'onclick=\"document.location='videogames/vg_home.html'\">Programming</div>\
             <div class='grid_attrib'>Animation</div>\
